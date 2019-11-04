01408ce4823ca9d97d2740ab058cd1ff9d2c86f8
Ilari Junnonen <ilari.junnonen@iki.fi>
2019-11-05 00:02:34 +0200

    Add viewing of individual recipes via a listing Update recipe creation

--- application/recipes/models.py
+++ application/recipes/models.py
@@ -1,25 +1,34 @@
 from application import db
 from application.ingredients import models
 
+# Change to db.Model() to access 'amount'
+#recipe_ingredients = db.Table('recipe_ingredient',
+#                             db.Column('recipe_id', db.Integer, db.ForeignKey(
+#                                 'recipe.id'), primary_key=True),
+#                             db.Column('ingredient_id', db.Integer, db.ForeignKey(
+#                                 'ingredient.id'), primary_key=True),
+#                             db.Column('amount', db.Integer)
+#                             )
 
-recipeingredients = db.Table('recipeingredient',
-                             db.Column('recipe_id', db.Integer, db.ForeignKey(
-                                 'recipe.id'), primary_key=True),
-                             db.Column('ingredient_id', db.Integer, db.ForeignKey(
-                                 'ingredient.id'), primary_key=True),
-                             db.Column('amount', db.Integer)
-                             )
+
+class RecipeIngredient(db.Model):
+    recipe_id = db.Column(db.Integer, db.ForeignKey('recipe.id'), primary_key=True)
+    ingredient_id = db.Column(db.Integer, db.ForeignKey('ingredient.id'), primary_key=True)
+    amount = db.Column(db.Integer)
 
 class Recipe(db.Model):
     id = db.Column(db.Integer, primary_key=True)
 
     name = db.Column(db.String(144), nullable=False)
+    description = db.Column(db.String(), nullable=True)
     instructions = db.Column(db.Text, nullable=True)
     time = db.Column(db.Integer, nullable=True)
     date_created = db.Column(db.DateTime, default=db.func.current_timestamp())
 
-    ingredients = db.relationship('Ingredient', secondary=recipeingredients, lazy='subquery',
-        backref=db.backref('recipes', lazy=True))
+    ingredients = db.relationship("RecipeIngredient")
+
+    #ingredients = db.relationship('Ingredient', secondary=recipe_ingredients, lazy='subquery',
+    #    backref=db.backref('recipes', lazy=True))
  
     def __init__(self, name):
         self.name = name
--- application/recipes/views.py
+++ application/recipes/views.py
@@ -1,22 +1,63 @@
 from application import app, db
 from flask import render_template, request, redirect, url_for
 from application.ingredients.models import Ingredient
-from application.recipes.models import Recipe
+from application.recipes.models import Recipe, RecipeIngredient
 
-r = Recipe("")
 ingredients_temp = {}
+id_temp = {}
+
+
+@app.route("/recipes/create", methods=["POST"])
+def create_recipe():
+    r = Recipe(name=request.form.get("name"))
+    r.instructions = request.form.get("instructions")
+    db.session().add(r)
+    db.session().flush()
+    for key, value in id_temp.items():
+        ri = RecipeIngredient(recipe_id=r.id, ingredient_id=key, amount=value)
+        db.session().add(ri)
+    db.session().commit()
+    ingredients_temp.clear()
+    id_temp.clear()
+    return redirect(url_for("recipe_form"))
+
 
 @app.route("/recipes/new", methods=["GET"])
 def recipe_form():
     return render_template("recipes/new.html", ingredients=Ingredient.query.all(), ingredients_temp=ingredients_temp)
 
+
 @app.route("/recipes/new", methods=["POST"])
 def add_to_recipe():
     i = Ingredient.query.get(request.form.get("id"))
     ingredients_temp[i] = request.form.get("amount")
-    return render_template("recipes/new.html", ingredients=Ingredient.query.all(), ingredients_temp=ingredients_temp)
+    id_temp[i.id] = request.form.get("amount")
+    return redirect(url_for("recipe_form"))
+
 
 @app.route("/recipes/clear", methods=["POST"])
 def clear_selection():
-    ingredients_temp.clear()
+    key_id = int(request.form.get("key_id"))
+    delete = None
+    for key, value in ingredients_temp.items():
+        if key.id == key_id:
+            delete = key
+            del id_temp[key.id]
+    del ingredients_temp[delete]
     return redirect(url_for("recipe_form"))
+
+
+@app.route("/recipes/<recipe_id>", methods=["GET"])
+def view_recipe(recipe_id):
+    r = Recipe.query.get(recipe_id)
+    r_i = RecipeIngredient.query.filter_by(recipe_id=r.id).all()
+    ingredients = {}
+    for item in r_i:
+        i = Ingredient.query.get(item.ingredient_id)
+        ingredients[i] = item.amount
+    return render_template("recipes/view.html", recipe=r, ingredients=ingredients)
+
+
+@app.route("/recipes/", methods=["GET"])
+def recipes_index():
+    return render_template("recipes/list.html", recipes=Recipe.query.all())
