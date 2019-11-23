794c1c5f1c147d91c4f0a1d0d4e82ff238986e16
Ilari Junnonen <ilari.junnonen@iki.fi>
2019-11-23 02:58:54 +0200

    Update signup alerts

--- application/auth/forms.py
+++ application/auth/forms.py
@@ -12,12 +12,13 @@ class LoginForm(FlaskForm):
 
 class UserForm(FlaskForm):
     name = StringField("Name", [validators.Length(
-        min=2, max=30, message="Name must be between 2 and 30 characters"),
-        validators.Regexp('^\w+$', message="Name can only contain alphanumeric characters")])
+        min=2, max=30, message="Name must be between 2 and 30 characters."),
+        validators.Regexp('^\w+$', message="Name can only contain alphanumeric characters.")])
     username = StringField("Username", [validators.Length(
-        min=4, max=20, message="Username must be between 4 and 20 characters"),
-        validators.Regexp('^\w+$', message="Username can only contain alphanumeric characters")])
-    password = PasswordField("Password", [validators.Length(min=4)])
+        min=4, max=20, message="Username must be between 4 and 20 characters."),
+        validators.Regexp('^\w+$', message="Username can only contain alphanumeric characters.")])
+    password = PasswordField("Password", [validators.Length(
+        min=4, max=30, message="Password must be between 4 and 30 characters.")])
 
     class Meta:
         csrf = False
